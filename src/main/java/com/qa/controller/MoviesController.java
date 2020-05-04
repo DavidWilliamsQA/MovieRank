36bc1518821369604917dca91d355990b1971387
David <temiloluwaw@gmail.com>
2020-05-04 12:47:55 +0100

    implemented all controllers implemented the application properties file

---
+++ src/main/java/com/qa/controller/MoviesController.java
@@ -0,0 +1,50 @@
+package com.qa.controller;
+
+import com.qa.domain.Movies;
+import com.qa.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
+
+@RestController
+public class MoviesController {
+
+    private final MovieService service;
+
+    @Autowired
+    public MoviesController(MovieService movie) {
+        this.service = movie;
+    }
+
+    @GetMapping("/getAllMovies")
+    public List<Movies> getAllMovies(){
+        return this.service.readMovies();
+    }
+
+    @PostMapping("/createMovies")
+    public Movies createMovie(@RequestBody Movies movie){
+        return this.service.createMovie(movie);
+    }
+
+    @DeleteMapping("/deleteMovie")
+    public boolean deleteMovie(@PathVariable Long id){
+        return this.service.deleteMovie(id);
+    }
+
+    @GetMapping("/getMovieById/{id}")
+    public Movies getMovieById(@PathVariable Long id){
+        return this.service.findMovieById(id);
+    }
+
+    @PutMapping("/updateMovie/{id}")
+    public Movies updateMovieById(@PathVariable Long id, @RequestBody Movies movie){
+        return this.service.updateMovie(id,movie);
+    }
+
+    @PutMapping("/updateMovie2")
+    public Movies updateMovies(@PathParam("id") Long id, @RequestBody Movies movie){
+        return this.service.updateMovie(id, movie);
+    }
+}
