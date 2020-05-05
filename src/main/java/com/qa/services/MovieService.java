e7400617e0794c6aeb8535753ed41ce860c6062a
David <temiloluwaw@gmail.com>
2020-05-05 15:27:15 +0100

    movie service refactored to use the dto's

--- src/main/java/com/qa/services/MovieService.java
+++ src/main/java/com/qa/services/MovieService.java
@@ -1,49 +1,61 @@
 package com.qa.services;
 
 import com.qa.domain.Movies;
+import com.qa.dto.MovieDTO;
 import com.qa.exceptions.MovieNotFoundException;
 import com.qa.repo.MoviesRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class MovieService {
 
-    private final MoviesRepository movie;
+    private final MoviesRepository repo;
+
+    private final ModelMapper mapper;
+
 
     @Autowired
-    public MovieService(MoviesRepository movie) {
-        this.movie = movie;
+    public MovieService(MoviesRepository movie, ModelMapper mapper) {
+        this.repo = movie;
+        this.mapper = mapper;
     }
 
-    public List<Movies> readMovies() {
-        return this.movie.findAll();
+    private MovieDTO mapToDTO(Movies movies){
+        return this.mapper.map(movies, MovieDTO.class);
     }
 
-    public Movies createMovie(Movies movies) {
-        return this.movie.save(movies);
+    public List<MovieDTO> readMovies() {
+        return this.repo.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
     }
 
-    public Movies findMovieById(Long id) {
-        return this.movie.findById(id).orElseThrow(MovieNotFoundException::new);
+    public MovieDTO createMovie(Movies movies) {
+        Movies tempMovie = this.repo.save(movies);
+        return this.mapToDTO(tempMovie);
     }
 
-    public Movies updateMovie(Long id, Movies movies){
-        Movies update = findMovieById(id);
+    public MovieDTO findMovieById(Long id) {
+        return this.mapToDTO(repo.findById(id).orElseThrow(MovieNotFoundException::new));
+    }
+
+    public MovieDTO updateMovie(Long id, Movies movies){
+        Movies update = this.repo.findById(id).orElseThrow(MovieNotFoundException::new);
         update.setTitle(movies.getTitle());
         update.setReleaseYear(movies.getReleaseYear());
         update.setDirector(movies.getDirector());
-        return this.movie.save(update);
+        return this.mapToDTO(update);
     }
 
     public boolean deleteMovie(Long id){
-        if(!this.movie.existsById(id)){
+        if(!this.repo.existsById(id)){
             throw new MovieNotFoundException();
         }
-        this.movie.deleteById(id);
-        return this.movie.existsById(id);
+        this.repo.deleteById(id);
+        return this.repo.existsById(id);
     }
 
 }
